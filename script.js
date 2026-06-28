90b7f2c1df83628fccf41d45cf5dab6e606b83e3
ghoulemblg44 <ghoulem.belgour@gmail.com>
2026-06-28 12:27:43 +0100

    update

--- script.js
+++ script.js
@@ -94,3 +94,23 @@ function filterProducts(category){
     });
 
 }
+
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+setInterval(() => {
+
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+
+        currentSlide = 0;
+
+    }
+
+    slides[currentSlide].classList.add("active");
+
+},4000);
